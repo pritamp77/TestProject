eb56ee0bafc01286e745675062ab21220b4f4674
Pritam <Pritam@192.168.0.104>
2018-09-10 23:24:54 +0530

    First Commit

---
+++ JDBCSpringAnnotation/src/main/java/com/pritam/JDBCSpringAnnotation/App.java
@@ -0,0 +1,26 @@
+package com.pritam.JDBCSpringAnnotation;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+                SpringJDBCConfiguration.class);
+ 
+        EmployeeDAO empDAO = applicationContext.getBean(EmployeeDAO.class);
+ 
+        String empName = empDAO.getEmployeeName(1);
+ 
+        System.out.println("Employee name is " + empName);
+ 
+        applicationContext.close();
+    
+    }
+}
---
+++ JDBCSpringAnnotation/src/main/java/com/pritam/JDBCSpringAnnotation/EmployeeDAO.java
@@ -0,0 +1,6 @@
+package com.pritam.JDBCSpringAnnotation;
+
+public interface EmployeeDAO {
+
+	public String getEmployeeName(int id);
+}
